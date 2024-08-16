fa4010c21539df4040896d5b937695c8925f1371
sarojini <sarojinielumalai91@gmail.com>
2024-08-16 16:21:34 +0530

    components are added

---
+++ src/components/lecture/lecture.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lecture',
+  standalone: true,
+  imports: [],
+  templateUrl: './lecture.component.html',
+  styleUrl: './lecture.component.scss'
+})
+export class LectureComponent {
+
+}

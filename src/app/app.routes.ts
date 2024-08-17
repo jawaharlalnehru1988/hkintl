16980d7ceecba24bd4321bbd73f947cc2d09e738
nehru00123@gmail.com <nehru00123@gmail.com>
2024-08-17 19:19:12 +0530

    routing is regularized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,36 +1,29 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from '../components/landing/landing.component';
-import { ArticlesComponent } from '../components/articles/articles.component';
 import { NgModule } from '@angular/core';
+import { EventsComponent } from '../components/events/events.component';
+import { ArticlesComponent } from '../components/articles/articles.component';
 import { AudioComponent } from '../components/audio/audio.component';
 import { VideoComponent } from '../components/video/video.component';
 import { LectureComponent } from '../components/lecture/lecture.component';
-import { EventsComponent } from '../components/events/events.component';
 
 export const routes: Routes = [
     {
-        path: '', component: LandingComponent
-    },
-    {
-        path: 'events', component: EventsComponent
-    },
-    {
-        path: 'article', component: ArticlesComponent
-    },
-    {
-        path: 'audio', component: AudioComponent
-    },
-    {
-        path: 'video', component: VideoComponent
-    },
-    {
-        path: 'lecture', component: LectureComponent
+        path: '', component: LandingComponent,
+        children: [
+            { path: '', redirectTo: 'events', pathMatch: 'full' },
+            { path: 'landing', component: LandingComponent },
+            { path: 'events', component: EventsComponent },
+            { path: 'article', component: ArticlesComponent },
+            { path: 'audio', component: AudioComponent },
+            { path: 'video', component: VideoComponent },
+            { path: 'lecture', component: LectureComponent },
+        ]
     }
 ];
-
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
+})
+export class AppRoutingModule { }

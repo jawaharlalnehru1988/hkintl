a642bbee643be678990dd4c12a1f10323b525ce2
sarojini <sarojinielumalai91@gmail.com>
2024-09-03 06:53:02 +0530

    articles are added

--- src/components/articles/articles.component.ts
+++ src/components/articles/articles.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './articles.component.scss'
 })
 export class ArticlesComponent {
+  currentPage: number = 0; // Start from the first article
+  articles = [1, 2, 3, 4, 5, 6, 7]; // Array representing each article
+  nextPage() {
+    
+    if (this.currentPage < this.articles.length - 1) {
+      this.currentPage++;
+    }
+  }
 
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
 }

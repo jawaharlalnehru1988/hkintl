0b20c27dbed539955cb70eda99dfe038d40c86ff
nehru00123@gmail.com <nehru00123@gmail.com>
2024-09-18 08:24:59 +0530

    article has been changed

--- src/components/events/events.component.ts
+++ src/components/events/events.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+interface Event {
+  date: string;
+  name: string;
+}
 @Component({
   selector: 'app-events',
   standalone: true,
@@ -8,5 +11,12 @@ import { Component } from '@angular/core';
   styleUrl: './events.component.scss'
 })
 export class EventsComponent {
-
+  events: Event[] = [
+    { date: '11-Sep-2024', name: 'Janmashtami Celebration' },
+    { date: '15-Sep-2024', name: 'Radha Ashtami' },
+    { date: '18-Sep-2024', name: 'Bhagavad Gita Recitation' },
+    { date: '21-Sep-2024', name: 'Krishna Kirtan Night' },
+    { date: '25-Sep-2024', name: 'Vishnu Sahasranama Chanting' },
+    { date: '30-Sep-2024', name: 'Krishna Consciousness Workshop' }
+  ];
 }
